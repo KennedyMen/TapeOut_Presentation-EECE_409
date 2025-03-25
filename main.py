5bd8dc6b3e9e5f85ec832c3f85dae6f92a642f9b
KennedyMen <kennedy.mensah@bison.howard.edu>
2025-03-25 14:52:26 -0400

    added a set of images to be processes

--- main.py
+++ main.py
@@ -1,33 +1,12 @@
-import manim
-import numpy
+import sys
+
+import numpy as np
 from manim import *
+from numpy import asarray
+from PIL import Image
+
+
 class ImageGrid(Scene):
     def construct(self):
-        # Load the image
-        image = ImageMobject("path/to/your/image.png")
-        image.scale(0.5)  # Scale the image to fit the scene
-
-        # Create a grid
-        rows, cols = 16, 16
-        grid = VGroup()
-        for i in range(rows):
-            for j in range(cols):
-                rect = Square(side_length=image.get_height() / rows)
-                rect.set_stroke(WHITE, width=1)
-                rect.move_to(
-                    image.get_corner(UL)
-                    + np.array([rect.get_width() * j, -rect.get_height() * i, 0])
-                )
-                grid.add(rect)
-
-        # Add the image and grid to the scene
-        self.add(image, grid)
-
-
-if __name__ == "__main__":
-    from manim import config
-
-    config.media_width = "100%"
-    config.quality = "high_quality"
-    scene = ImageGrid()
-    scene.render()
+        img = ImageMobject("images/HollowRed.png")
+        self.add(img)
