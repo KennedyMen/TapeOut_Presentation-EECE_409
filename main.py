6882c2c2c728aebada9e51aac6f677370bf15358
KennedyMen <kennedy.mensah@bison.howard.edu>
2025-03-25 18:08:31 -0400

    got baby steps for animating a pixel image
    basic setup for moving around an image getting ready for presenting the
    finshed work

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
-
 import numpy as np
 from manim import *
-from numpy import asarray
 from PIL import Image
-
+from manim_slides import Slide
 
 class ImageGrid(Scene):
     def construct(self):
-        img = ImageMobject("images/HollowRed.png")
+        start_img = Image.open("/home/deck/Documents/Projects/Python/Presentation_manim/media/images/image_Processing/Hollow.png").convert('L')
+        color_img = ImageMobject("media/images/image_Processing/Hollow.png")
+        img = ImageMobject(np.array(start_img))
         self.add(img)
+        self.wait(0.5)
+        self.play(img.animate.scale(18))
+        self.wait(0.5)
+        self.play(img.animate.shift(LEFT*5))
+
